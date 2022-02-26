e7ab91798a60ff1e09160c5628bef060dfda64d5
Nikolaos Chatzikonstantinou <nchatz314@gmail.com>
2022-02-26 19:17:34 +0900

    fix(ysign): Remove nonsense options.
    When `--clearsign` is used, the option `--armor` is implied, and the
    signature cannot be detached.

--- ysign.sh
+++ ysign.sh
@@ -3,7 +3,7 @@
 if [ $# -lt 1 ]
 then
     printf "%s [files...]\n\nSigns the provided files.\n\n" "$(basename $0)"
-    printf "Runs:\n    gpg --clearsign --detach-sig --armor --output file.sig file\n"
+    printf "Runs:\n    gpg --clearsign --output file.sig file\n"
     exit 0
 fi
 
@@ -11,6 +11,6 @@ for input
 do
     output="$input.sig"
     printf "Signing %s... " "$input"
-    gpg --clearsign --detach-sig --armor --output "$output" "$input" &&
+    gpg --clearsign --output "$output" "$input" &&
     printf "done:\n%s -> %s\n" "$input" "$output"
 done
